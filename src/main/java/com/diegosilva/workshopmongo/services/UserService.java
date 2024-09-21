071d1084a883e6ab2a135a025668dc073dfae70f
Diego Silva <diegossilva2008@gmail.com>
2024-09-21 10:10:47 -0300

    Obtendo um usuário por id

--- src/main/java/com/diegosilva/workshopmongo/services/UserService.java
+++ src/main/java/com/diegosilva/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.diegosilva.workshopmongo.services;
 
 import com.diegosilva.workshopmongo.domain.User;
 import com.diegosilva.workshopmongo.repository.UserRepository;
+import com.diegosilva.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
